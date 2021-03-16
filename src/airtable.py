a7a5e78d3bc2db822bae732debc9bb3385eece2e
Marco Wang <xwang423@fordham.edu>
2021-03-15 20:40:08 -0400

    practise

--- src/airtable.py
+++ src/airtable.py
@@ -1,32 +1,34 @@
 import os
 import requests
+from dataclasses import dataclass
 
-AIRTABLE_BASE_ID = os.environ.get("AIRTABLE_BASE_ID")
-AIRTABLE_API_KEY = os.environ.get("AIRTABLE_API_KEY")
-AIRTABLE_TABLE_NAME = os.environ.get("AIRTABLE_TABLE_NAME")
 
-def push_to_airtable(email=None):
-    if email is None:
-        return False
-    end_point = f"https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}"
 
-    header = {
-        "Authorization": "Bearer {AIRTABLE_API_KEY}" ,
-        "Content-Type": "application/json"
-    }
+@dataclass()
+class Airtable:
+    base_id:str 
+    api_key:str
+    table_name:str
 
-    data = {
-    "records": [
-        {
-        "fields": {}
-        },
-        {
-        "fields": {
-            "email_added":email
+    def create_records(self, data = {}):
+        if len(data) == 0:
+            return False
+        end_point = f"https://api.airtable.com/v0/{self.base_id}/{self.table_name}"
+
+        header = {
+            "Authorization": f"Bearer {self.api_key}" ,
+            "Content-Type": "application/json"
         }
-        }
-    ]
-    }
 
-    r = requests.post(end_point, json = data, headers= header)
-    return r.status_code == 200 or r.status_code == 201
+        json_data = {
+        "records": [
+            {
+            "fields": data
+            }
+        ]
+        }
+
+        r = requests.post(end_point, json = json_data, headers= header)
+        print(end_point, r.json())
+
+        return r.status_code == 200 or r.status_code == 201
--- src/app.py
+++ src/app.py
@@ -3,7 +3,8 @@ from functools import lru_cache
 import pathlib
 from fastapi import FastAPI,Request,Form 
 from fastapi.templating import Jinja2Templates
-from .airtable import push_to_airtable
+from .airtable import Airtable
+import os
 
 BASE_DIR = pathlib.Path(__file__).parent ## this is the src/ 
 
@@ -18,6 +19,16 @@ def cached_dotenv():
 
 cached_dotenv()
 
+
+
+AIRTABLE_BASE_ID = os.environ.get("AIRTABLE_BASE_ID")
+AIRTABLE_API_KEY = os.environ.get("AIRTABLE_API_KEY")
+AIRTABLE_TABLE_NAME = os.environ.get("AIRTABLE_TABLE_NAME")
+
+
+
+
+
 @app.get("/")
 def home_view(request: Request):
     return  templates.TemplateResponse("home.html", {"request": request})
@@ -25,7 +36,14 @@ def home_view(request: Request):
 @app.post("/")
 def home_signup_view(request: Request, email:str = Form(...)):
 
-    did_send = push_to_airtable(email=email)
+
+    airtable_client = Airtable(
+        base_id = AIRTABLE_BASE_ID,
+        api_key = AIRTABLE_API_KEY,
+        table_name = AIRTABLE_TABLE_NAME,
+    )
+            
+    did_send = airtable_client.create_records({"email_added":email})
 
     return templates.TemplateResponse("home.html", {
         "request": request,
